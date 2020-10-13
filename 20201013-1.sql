472da13210b78ca32fffc655272aaf83c1df9ad0
Jener Sigua <siguajener@yahoo.com>
2020-10-13 10:27:23 +0800

    delete attributes

---
+++ 20201013-1.sql
@@ -0,0 +1,9 @@
+DELETE FROM `uam`.`attributes_values` WHERE (`id` = '1');
+DELETE FROM `uam`.`attributes_values` WHERE (`id` = '2');
+DELETE FROM `uam`.`attributes_values` WHERE (`id` = '3');
+DELETE FROM `uam`.`attributes_values` WHERE (`id` = '4');
+DELETE FROM `uam`.`attributes_values` WHERE (`id` = '5');
+DELETE FROM `uam`.`attributes_values` WHERE (`id` = '6');
+
+DELETE FROM `uam`.`category_attributes` WHERE (`category_id` = '5') and (`category_value` = 'Teams');
+DELETE FROM `uam`.`category_attributes` WHERE (`category_id` = '6') and (`category_value` = 'Profile ID');
